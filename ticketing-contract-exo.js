9b2288bca9756e8e44495b6103875ef72b98652c
Mario Colombo <mario@what.digital>
2019-06-22 20:16:22 +0200

    new ticketing sc

--- ticketing-contract-exo.js
+++ ticketing-contract-exo.js
@@ -2,5 +2,5 @@
 
 module.exports = {
     WS_ADDRESS: 'wss://eth-testnode.what.digital',
-    SC_TICKETNG_ADDRESS: "0x26b4AFb60d6C903165150C6F0AA14F8016bE4aec",
+    SC_TICKETNG_ADDRESS: "0x49455287eBCC42a875B40724C62519D03e2EDcff",
 };
